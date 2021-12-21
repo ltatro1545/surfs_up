cbc1cc3b1952311da5a254e60f52273c027f4f1f
ltatro1545 <92493572+ltatro1545@users.noreply.github.com>
2021-12-20 19:34:59 -0500

    updated app code

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
 # import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask
+#########################
+
+# Set up the Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+
+# Reflect the table
+Base.prepare(engine, reflect=True)
+
+# Create variables for two classes: measurement and station
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link from Python to the database
+session = Session(engine)
+##########################
 
 # Create a Flask instance
 app = Flask(__name__)
 
 # create Flask route
 @app.route('/')
-def hello_world():
-    return "Hello world"
+def welcome():
+    return (
+    '''
+    Welcome to the Hawaii Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
-# Run Flask app
+# Define main behavior
+if __name__ == "__main__":
+    app.run(debug=True)
