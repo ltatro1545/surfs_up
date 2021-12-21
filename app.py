c590935912ade1882cfb791b7230802805cb8aba
ltatro1545 <92493572+ltatro1545@users.noreply.github.com>
2021-12-21 13:25:49 -0500

    added remaining routes to flask app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask
+from flask import Flask, jsonify
 #########################
 
 # Set up the Database
@@ -29,7 +29,7 @@ session = Session(engine)
 # Create a Flask instance
 app = Flask(__name__)
 
-# create Flask route
+# create home Flask route
 @app.route('/')
 def welcome():
     return (
@@ -41,6 +41,62 @@ def welcome():
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
+
+# Create precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # prev year is equal to current date - 365 days
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # query for precip data from the past year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    # jsonify results
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+# Create Stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Put stations in list
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    # jsonify results (station=station) allows jsonify to function here
+    return jsonify(stations=stations)
+
+# Create monthly temperature route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    # prev year is equal to current date minus 365 days
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # query primary station for all observations in last year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    # unravel results into one dimensional array, then convert to list
+    temps = list(np.ravel(results))
+    # jsonify list and return results
+    return jsonify(temps=temps)
+
+# Create statistics route that allows you to input the start and end date in the route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    # create a list of measured data to query with
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    # query selection and ravel results, then jsonify the return
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
 
 # Define main behavior
 if __name__ == "__main__":
